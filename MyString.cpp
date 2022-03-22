b6bab13b979a7161e21c11481b8844ad49911e14
Mason <73012906+masonhgn@users.noreply.github.com>
2022-03-22 18:35:33 -0400

    Update MyString.cpp

--- MyString.cpp
+++ MyString.cpp
@@ -14,8 +14,6 @@ MyString::MyString(const char* c) {
     size = strlen(c);
     chars = new char[size+1]; //accounts for extra /n char in cstring library
     strcpy(chars, c);
-
-
 }
 
 MyString::MyString(int i ) {
@@ -27,17 +25,39 @@ MyString::~MyString() {
 	delete[] chars;
 }
 
+
+void MyString::grow(unsigned int sz) {
+    char* ch;
+    size += sz; //change size to necessary amount
+    ch = new char[size]; //create temp array
+    strcpy(ch, chars); //copy main array to temp
+    delete[] chars; //delete old array
+    chars = new char[size]; //remake initial array
+    strcpy(chars, ch); //copy from temp
+    delete[] ch;
+}
+
 MyString::MyString(const MyString& s) {
 //copy constructor
-
+    size = s.size;
+    chars = new char[s.size+1];
+    strcpy(chars, s.chars);
 }
 
 MyString& MyString::operator=(const MyString& s) {
 //operator=
+    if (s.size != size) {
+        delete[] chars;
+        chars = new char[s.size];
+    }
+
+    strcpy(chars, s.chars);
 }
 
 MyString& MyString::operator+=(const MyString& s) {
 //operator+=
+    grow(s.size);
+    strcat(chars, s.chars);
 }
 
 char& MyString::operator[](unsigned int index) {
@@ -46,6 +66,8 @@ char& MyString::operator[](unsigned int index) {
 
 const char& MyString::operator[](unsigned int index) const {
 //operator[] for retrieving element at index of string
+    //check if index is in bounds
+    return chars[index];
 }
 
 MyString& MyString::insert(unsigned int index, const MyString& s) {
@@ -53,7 +75,7 @@ MyString& MyString::insert(unsigned int index, const MyString& s) {
 }
 
 int MyString::indexOf(const MyString& s) const {
-//return index of string char
+//return index of substring with in string object
 }
 
 int MyString::getLength() const {
@@ -87,6 +109,7 @@ ostream& operator<<(ostream& o, const MyString& s) {
 
 istream& operator>>(istream& i, MyString& s) {
 //extraction operator
+    //getline()??
 return i;
 }
 
@@ -100,33 +123,37 @@ istream& getLine(istream& i, MyString& s, char delim) {
 
 MyString operator+(const MyString& a, const MyString& b) {
 //operator+
+    //use strcat
 }
 
 
 
 
 
-bool operator< (const MyString& , const MyString& ) {
+bool operator< (const MyString& x, const MyString& y) {
 //operator<
+    if (x.size < y.size) return true;
+    else return false;
+    //is this right?
 }
 
-bool operator> (const MyString& , const MyString& ) {
+bool operator> (const MyString& x, const MyString& y) {
 //operator>
 }
 
-bool operator<=(const MyString& , const MyString& ) {
+bool operator<=(const MyString& x, const MyString& y) {
 //operator<=
 }
 
-bool operator>=(const MyString& , const MyString& ) {
+bool operator>=(const MyString& x, const MyString& y) {
 //operator>=
 }
 
-bool operator==(const MyString& , const MyString& ) {
+bool operator==(const MyString& x, const MyString& y) {
 //operator==
 }
 
-bool operator!=(const MyString& , const MyString& ) {
+bool operator!=(const MyString& x, const MyString& y) {
 //operator!=
 }
 
